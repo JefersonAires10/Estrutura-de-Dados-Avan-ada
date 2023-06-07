6cb8415877531734032bd7d3dc5ad08ed3f80503
Jeferson Aires <111990809+JefersonAires10@users.noreply.github.com>
2023-06-07 17:21:37 -0300

    priority_queue

---
+++ Priority Queue/Atividade01/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include "PriorityQueue.h"
+
+int main() {
+
+    vector<int> vec={1,2,3,4,5,6,7,8,9,10};
+    
+    PriorityQueue<int> Queue(vec);
+
+    cout << fila.top() << endl;
+
+}
